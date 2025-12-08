1075d4d7ad10690b6ebc8b6eaaa041a6765c16eb
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-12-09 00:59:48 +0100

    update demo first selection

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   type BoardModel,
   BoardViewComponent,
   type ClickedBoardEvent,
+  type PlayerBoardModel,
 } from "./view/board.ts";
 import type { DefineComponentFunc } from "./view/index.ts";
 import { ItemsViewComponent } from "./view/items.ts";
@@ -21,8 +22,20 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   </div>
 `;
 
+const players: { [id: string]: PlayerBoardModel } = {
+  p0: { id: "p0", position: [0, 0], asset: 0 },
+  p1: { id: "p1", position: [1, 0], asset: 1 },
+  p2: { id: "p2", position: [2, 0], asset: 2 },
+  p3: { id: "p3", position: [3, 0], asset: 3 },
+  p4: { id: "p4", position: [4, 0], asset: 4 },
+  p5: { id: "p5", position: [5, 0], asset: 5 },
+  p6: { id: "p6", position: [6, 0], asset: 6 },
+  p7: { id: "p7", position: [7, 0], asset: 7 },
+  p8: { id: "p8", position: [8, 0], asset: 8 },
+};
+
 const boardModel: BoardModel = {
-  players: [{ id: "p1", position: [10, 10], asset: 0 }],
+  players: Object.values(players),
   selection: [],
 };
 const boardComponent = document.querySelector<BoardViewComponent>(
@@ -47,7 +60,8 @@ boardComponent?.addEventListener(
     ) {
       boardModel.selection.push({ position: [x, y] });
     }
-    boardModel.players = [{ id: "red", asset: 1, position: [x, y] }];
+    players.p8.position = [x, y];
+    boardModel.players = Object.values(players);
     boardComponent.updateModel(boardModel);
   },
 );
