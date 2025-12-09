80e83215634fb186f7f385ff4c39c895d04979cb
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-12-09 03:26:07 +0100

    add check on movement

--- src/main.ts
+++ src/main.ts
@@ -27,11 +27,11 @@ const players: { [id: string]: PlayerBoardModel } = {
   p0: { id: "p0", position: [0, 0], asset: 0 },
   p1: { id: "p1", position: [1, 0], asset: 1 },
   p2: { id: "p2", position: [2, 0], asset: 2 },
-  p3: { id: "p3", position: [3, 0], asset: 3 },
-  p4: { id: "p4", position: [4, 0], asset: 4 },
-  p5: { id: "p5", position: [5, 0], asset: 5 },
-  p6: { id: "p6", position: [6, 0], asset: 6 },
-  p7: { id: "p7", position: [7, 0], asset: 7 },
+  p3: { id: "p3", position: [0, 17], asset: 3 },
+  p4: { id: "p4", position: [7, 24], asset: 4 },
+  p5: { id: "p5", position: [24, 18], asset: 5 },
+  p6: { id: "p6", position: [24, 6], asset: 6 },
+  p7: { id: "p7", position: [15, 0], asset: 7 },
   p8: { id: "p8", position: [8, 0], asset: 8 },
 };
 
@@ -40,7 +40,12 @@ const map = getMap();
 const boardModel: BoardModel = {
   players: Object.values(players),
   selection: [],
-  highlight: [],
+  highlight: findNearValidPlayerBlock(
+    players.p8.position[0],
+    players.p8.position[1],
+  ).map((
+    [x, y],
+  ) => ({ position: [x, y] })),
 };
 const boardComponent = document.querySelector<BoardViewComponent>(
   "board-component",
@@ -54,6 +59,14 @@ boardComponent?.addEventListener(
   (event) => {
     const pos = (event as CustomEvent<ClickedBoardEvent>).detail.position;
     const [x, y] = pos;
+    console.log("X, Y", x, y);
+    if (
+      !boardModel.highlight.map((b) => b.position).find(([xh, yh]) =>
+        xh == x && yh == y
+      )
+    ) {
+      return;
+    }
     if (map[y][x] != "x" && map[y][x] != "S") {
       return;
     }
@@ -84,4 +97,10 @@ function findNearBlock(pointX: number, pointY: number) {
       pointX + x >= 0 && pointY + y >= 0 &&
       pointX + x < map[0].length && pointY + y < map.length
     ).map(([x, y]) => [pointX + x, pointY + y]);
+}
+
+function findNearValidPlayerBlock(x: number, y: number) {
+  return findNearBlock(x, y).filter((
+    [x, y],
+  ) => map[y][x] != " " && !map[y][x].startsWith("M"));
 }
