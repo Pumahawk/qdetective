3a8b421bf2ed71a6f40e748fc1bd5f5223a3dfb9
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-12-18 00:28:40 +0100

    Create ItemSelectionComponent

---
+++ src/components/ItemSelectionComponent.tsx
@@ -0,0 +1,45 @@
+import { getItemById } from "../services/AppService.ts";
+
+export interface ItemSelectionProps {
+  itemGroups: string[][];
+
+  onConfirm?: (items: string[]) => void;
+}
+
+export function ItemSelectionComponent(
+  { itemGroups, onConfirm }: ItemSelectionProps,
+) {
+  const itemGroupsInfo = itemGroups.map((
+    itemsId,
+  ) => (itemsId.map((id) => getItemById(id))));
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const selectedItems = retrieveSelectedItemsFromForm(e.currentTarget);
+        onConfirm && onConfirm(selectedItems);
+      }}
+    >
+      {itemGroupsInfo.map((group, i) => (
+        <div key={i}>
+          <select name={"item_" + i} required>
+            {group.map((item) => (
+              <option key={item.id} value={item.id}>{item.name}</option>
+            ))}
+          </select>
+        </div>
+      ))}
+      <button type="submit">Confirm</button>
+    </form>
+  );
+}
+
+function retrieveSelectedItemsFromForm(form: HTMLFormElement): string[] {
+  const data = new FormData(form);
+  const itemsIds: string[] = [];
+  for (const [key, value] of data.entries()) {
+    if (key.startsWith("item_")) itemsIds.push(value.toString());
+  }
+  return itemsIds;
+}
